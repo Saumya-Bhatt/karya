f29079889c2d73324b8f04b99a7a707ea53f37a1
Saumya Bhatt <108668549+SaumyaBhatt106@users.noreply.github.com>
2024-12-11 17:45:57 +0530

    Added support for redis cluster

--- data/redis/src/main/kotlin/karya/data/redis/configs/RedisLocksConfig.kt
+++ data/redis/src/main/kotlin/karya/data/redis/configs/RedisLocksConfig.kt
@@ -4,9 +4,14 @@ import karya.core.configs.LocksConfig
 import karya.core.utils.readValue
 
 data class RedisLocksConfig(
-  val host: String,
   val waitTime: Long,
   val leaseTime: Long,
+  val password: String,
+  val clusterNodes: List<String>,
+  val clusterMode: Boolean,
+  val connectionTimeout: Int,
+  val connectionPoolSize: Int,
+  val connectionMinimumIdleSize: Int
 ) : LocksConfig(REDIS_IDENTIFIER) {
 
   companion object {
@@ -14,8 +19,13 @@ data class RedisLocksConfig(
   }
 
   constructor(props: Map<*, *>) : this(
-    host = props.readValue("host"),
+    clusterNodes = props.readValue("clusterNodes"),
+    password = props.readValue("password"),
+    clusterMode = props.readValue("clusterMode"),
     leaseTime = props.readValue("leaseTime"),
     waitTime = props.readValue("waitTime"),
+    connectionTimeout = props.readValue("connection.timeout"),
+    connectionPoolSize = props.readValue("connection.poolSize"),
+    connectionMinimumIdleSize = props.readValue("connection.minimumIdleSize")
   )
 }
---
+++ docs/local-setup/cluster/redis/redis.sh
@@ -0,0 +1,24 @@
+ANNOUNCE_IP=$1
+ANNOUNCE_PORT=$(expr $2)
+ANNOUNCE_BUS_PORT=$(expr $ANNOUNCE_PORT + 100)
+
+CONF_FILE="/tmp/redis.conf"
+
+# generate redis.conf file
+echo "port 6379
+cluster-enabled yes
+cluster-config-file nodes.conf
+cluster-node-timeout 5000
+appendonly yes
+loglevel debug
+requirepass karya
+masterauth  karya
+protected-mode no
+maxclients 10000
+cluster-announce-ip $ANNOUNCE_IP
+cluster-announce-port $ANNOUNCE_PORT
+cluster-announce-bus-port $ANNOUNCE_BUS_PORT
+" >> $CONF_FILE
+
+# start server
+redis-server $CONF_FILE
