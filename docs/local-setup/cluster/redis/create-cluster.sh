21a955f0defdb8ed709588f5efba5644b5f86528
Saumya Bhatt <108668549+SaumyaBhatt106@users.noreply.github.com>
2024-12-11 18:26:14 +0530

    Creating redis cluster via script

---
+++ docs/local-setup/cluster/redis/create-cluster.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get the local IP address (works for macOS and Linux)
+LOCAL_IP=$1
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+COMPOSE_FILE_PATH="$PARENT_PATH/docs/local-setup/cluster/redis.docker-compose.yml"
+
+echo "COMPOSE_FILE_PATH: $COMPOSE_FILE_PATH"
+
+
+# Ensure LOCAL_IP is set
+if [ -z "$LOCAL_IP" ]; then
+  echo "Error: LOCAL_IP is not set. Could not determine local IP address."
+  exit 1
+fi
+
+echo "Using LOCAL_IP: $LOCAL_IP"
+
+# Run the redis-cli command inside the redis-node-1 container
+docker-compose -f $COMPOSE_FILE_PATH exec -T redis-node-1 redis-cli -a 'karya' \
+  --cluster create $LOCAL_IP:7001 $LOCAL_IP:7002 $LOCAL_IP:7003 \
+  --cluster-replicas 0 \
+  --cluster-yes
