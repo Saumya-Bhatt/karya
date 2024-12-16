0ba88b253798dd3d30381b65eb39edc83b1bbab4
Saumya Bhatt <108668549+SaumyaBhatt106@users.noreply.github.com>
2024-12-16 20:01:44 +0530

    Added sample to write to slack

---
+++ client/python-client/samples/make_recurring_slack_request.py
@@ -0,0 +1,58 @@
+import sys
+import os
+
+# Add the root directory of the project to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from karya.commons.config import ClientConfig
+from karya.commons.entities.requests import (
+    CreateUserRequest,
+    SubmitPlanRequest,
+)
+from karya.commons.entities.models.Action import SlackMessageRequest
+from karya.commons.entities.models.Plan import Recurring
+from karya.clients.KaryaRestClient import KaryaRestClient
+
+
+async def main():
+    config = ClientConfig.dev()
+    client = KaryaRestClient(config)
+
+    create_user_request = CreateUserRequest(name="python-client")
+    user = await client.create_user(create_user_request)
+    print(user)
+
+    slack_message = """[
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Hello, this is periodic slack message from Karya!"
+            }
+        }
+    ]"""
+
+    slack_action = SlackMessageRequest(
+        channel="C083L324V99",
+        message=slack_message,
+    )
+
+    plan_request = SubmitPlanRequest(
+        user_id=user.id,
+        description="Make a recurring API call from python client",
+        period_time="PT7S",
+        max_failure_retry=3,
+        plan_type=Recurring(
+            end_at=None,
+        ),
+        action=slack_action,
+    )
+
+    plan = await client.submit_plan(plan_request)
+    print(plan)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
